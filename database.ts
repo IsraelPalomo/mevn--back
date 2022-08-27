1a11756ed59b0521328ddc7812f4fc99fb330089
Israel <israelpalomoe@gmail.com>
2022-08-27 16:58:08 +0200

    first

--- database.ts
+++ database.ts
@@ -2,7 +2,9 @@ import { connect } from 'mongoose';
 
 export const StartConnection = async (): Promise<void> => {
   try {
-    const db = await connect('mongodb://localhost/mevn-database');
+    const db = await connect(
+      'mongodb+srv://israel:Alumno$1@cluster0.cdjqzap.mongodb.net/?retryWrites=true&w=majority'
+    );
     console.log(db.connection.name);
   } catch (error) {
     console.log(error);
